23f0f0c3b00b0697f48c2d23b5b3d2d9c862f924
Nahuel <nahuelescujuri@gmail.com>
2023-02-14 14:17:56 -0300

    DB_NAME add in db.js

--- api/index.js
+++ api/index.js
@@ -23,12 +23,12 @@ const { conn, Diet, Recipe } = require('./src/db.js');
 let allDiets = ["gluten free","ketogenic","vegetarian","lacto-vegetarian","ovo-vegetarian","vegan","pescetarian","paleo","primal","FODMAP","whole30"] 
 const preloadDiets = async ()=> allDiets.map(async r => await Diet.create({name:r}));
 
-const port = process.env.port || 3001;
+const {DB_PORT} = process.env
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(port, () => {
+  server.listen(DB_PORT, () => {
     preloadDiets();
-    console.log('%s listening at '+port); // eslint-disable-line no-console
+    console.log('%s listening at '+DB_PORT); // eslint-disable-line no-console
   });
 });
