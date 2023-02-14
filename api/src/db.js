61ca86bd3148d6fa8ca2305b5819cb03b022fe04
Nahuel <nahuelescujuri@gmail.com>
2023-02-14 15:10:55 -0300

    sequelize port add in db.js

--- api/src/db.js
+++ api/src/db.js
@@ -3,13 +3,16 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
+                              // postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  // port: DB_PORT,port = 2345
 });
+// console.log(sequelize)
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
