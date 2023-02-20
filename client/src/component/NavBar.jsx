9db145caa326e98f736592dbe6c5800d93796cce
Nahuel <nahuelescujuri@gmail.com>
2023-02-20 16:06:14 -0300

    NavBar style update

--- client/src/component/NavBar.jsx
+++ client/src/component/NavBar.jsx
@@ -10,10 +10,13 @@ export default function NavBar(){
 		{/* <!-- Nav --> */}
 		<div className="nav container">
 			{/* <!-- Logo --> */}
-            <Link className="logo" to={"/recipes"}>
-				Henry<span className='span1'>Food</span>
+            <Link className="logo" to={"/"}>
+				Pi <span className='span1'>Food</span>
 				</Link>
-			{/* <!-- Nav Icons -->			 */}
+			{/* <!-- Nav Icons -->*/}
+			<div className="nav-icons">
+				<Link to={"/about-me"} className="btn bx-ball" id="bell-icon">About Me</Link>
+			</div>
 			<div className="nav-icons">
 				<Link to={"/create"} className="btn bx-ball" id="bell-icon">Create</Link>
 			</div>
